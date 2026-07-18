dfd85a55fd6a6200f978a9a78e06757e04f22e62
moonlight169 <monlight169@gmail.com>
2026-07-19 00:14:49 +0700

    feat(config): add arm motor/limit-switch pins and master arm UART/relay pins
    Define STM32 arm slave pin map (Box/Arm/Lift motors + limit switches)
    in config_arm.h, and add ARM_UART/Relay1-6 pin definitions to
    config_esp32.h per Diagram_manual_robot.pdf.

---
+++ include/config_arm.h
@@ -0,0 +1,17 @@
+#define MotorBox_A PB0
+#define MotorBox_B PB1
+
+#define MotorArm_A PA6
+#define MotorArm_B PA7
+
+#define MotorLift_A PA2
+#define MotorLift_B PA3
+
+#define LimitSW_Box_Front PB12
+#define LimitSW_Box_Back PB13
+
+#define LimitSW_Arm_Front PB15
+#define LimitSW_Arm_Back PB14
+
+#define LimitSW_Lift_Up PB4
+#define LimitSW_Lift_Down PB5
--- include/config_esp32.h
+++ include/config_esp32.h
@@ -2,7 +2,20 @@
 #define MAC_PS5_RED "0c:27:56:1a:0c:6e"
 #define COMMAND_RATE 50
 
-// UART link -> 2_slave_wheel
+#define SCA 21
+#define SCL 22
+
 #define WHEEL_UART_RX 16
 #define WHEEL_UART_TX 17
 #define WHEEL_UART_BAUD 115200
+
+#define ARM_UART_RX 19
+#define ARM_UART_TX 18
+#define ARM_UART_BAUD 115200
+
+#define Relay1 32
+#define Relay2 33
+#define Relay3 25
+#define Relay4 26
+#define Relay5 27
+#define Relay6 14
